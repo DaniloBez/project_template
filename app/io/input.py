c517d8c9b9ae7d5ab57bc2f657c3f84ec70f9789
Danylo Bezukh <113092192+DaniloBez@users.noreply.github.com>
2026-02-27 20:10:18 +0200

    Add io functions

---
+++ app/io/input.py
@@ -0,0 +1,11 @@
+
+def input_from_console():
+    ...
+
+
+def input_from_file():
+    ...
+
+
+def input_from_file_pandas():
+    ...
---
+++ app/io/output.py
@@ -0,0 +1,12 @@
+
+
+def output_in_console():
+    ...
+
+
+def output_in_file():
+    ...
+
+
+def output_in_file_pandas():
+    ...
